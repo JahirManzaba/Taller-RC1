dfd6d999772ceff337c7c7e30a56fce6ed4fe42a
Martin <Martinleoncruz123@gmail.com>
2026-05-24 21:23:46 -0500

    arreglos del primer commit

--- Taller-RC1.c
+++ Taller-RC1.c
@@ -3,22 +3,22 @@
 
 int main() {
     int n;
-    printf("Ingrese el numero de estudiantes: ");
+    printf("Ingrese la cantidad de estudiantes: ");
     scanf("%d", &n);
 
     float notas[n][3];
-    float *ptr; // Puntero auxiliar
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         printf("Estudiante %d:\n", i + 1);
-        for (int j = 0; j < 3; j++) {
-            ptr = *(notas + i) + j; // Apunta a la celda específica
+        for(int j = 0; j < 3; j++) {
             do {
-                printf("  Asignatura %d (0-10): ", j + 1);
-                scanf("%f", ptr);
-                if (*ptr < 0 || *ptr > 10) printf("Nota invalida. ");
-            } while (*ptr < 0 || *ptr > 10);
+                printf("  Nota asignatura %d: ", j + 1);
+                scanf("%f", &notas[i][j]);
+                if(notas[i][j] < 0 || notas[i][j] > 10) 
+                    printf("  Error: La nota debe estar entre 0 y 10.\n");
+            } while(notas[i][j] < 0 || notas[i][j] > 10);
         }
     }
+
     return 0;
 }
