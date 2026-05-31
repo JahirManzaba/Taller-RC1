82ce42f4de5889a2ed4df323faadf98dc9742a3e
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-05-31 16:37:10 -0500

    agregamos reporte por estudiantes

--- Taller-RC1.c
+++ Taller-RC1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void calcularEstadisticasEstudiante(float notasEstudiante[], int cantidadNotas, float *promedio, float *max, float *min) {
+    float suma = 0;
+    *max = 0;
+    *min = 10;
+    for(int j = 0; j < cantidadNotas; j++) {
+        suma += notasEstudiante[j];
+        if(notasEstudiante[j] > *max) *max = notasEstudiante[j];
+        if(notasEstudiante[j] < *min) *min = notasEstudiante[j];
+    }
+    *promedio = suma / cantidadNotas;
+}
+
 int main() {
     int n;
     printf("Ingrese la cantidad de estudiantes: ");
@@ -18,6 +30,15 @@ int main() {
             } while(notas[i][j] < 0 || notas[i][j] > 10);
         }
     }
+
+    printf("\n=== REPORTE POR ESTUDIANTE ===\n");
+    float promEstudiante, maxNotaEst, minNotaEst;
+    for(int i = 0; i < n; i++) {
+        calcularEstadisticasEstudiante(notas[i], 3, &promEstudiante, &maxNotaEst, &minNotaEst);
+        printf("Estudiante %d - Promedio: %.2f | Max: %.2f | Min: %.2f\n", 
+                i + 1, promEstudiante, maxNotaEst, minNotaEst);
+    }
+
 
     return 0;
 }
